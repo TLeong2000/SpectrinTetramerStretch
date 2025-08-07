ab133c63fdbdba96d34bd7952778ebbd0fdacd38
Timothy Leong <tleong4@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2025-08-07 04:03:42 +0000

    Many issues with fix_arc_length_restraint
    The force is not properly utilized by the minimizer.
    The force exerted on the atoms of a molecule during a run accumulate
    with each timestep.

--- IdealizedChainStretch/fix_arc_length_restraint.cpp
+++ IdealizedChainStretch/fix_arc_length_restraint.cpp
@@ -16,6 +16,7 @@
 #include "math.h"
 #include "stdlib.h"
 #include "atom.h"
+#include "comm.h"
 #include "molecule.h"
 #include "error.h"
 #include "modify.h"
@@ -35,6 +36,7 @@ using namespace FixConst;
 FixArcLengthRestraint::FixArcLengthRestraint(LAMMPS *lmp, int narg, char **arg) : 
    Fix(lmp, narg, arg) 
 {
+   global_freq = 1;
    scalar_flag = 1;
    energy_global_flag = 1;
    extscalar = 1;
@@ -96,7 +98,11 @@ int FixArcLengthRestraint::setmask()
 /* ---------------------------------------------------------------------- */
 
 void FixArcLengthRestraint::post_force(int /*vflag*/)
-{  
+{ 
+   int newton_bond = force->newton_bond;
+   if (newton_bond == 1) comm->reverse_comm(this);
+
+   printf("\n### Begin calculation of restraint forces called by post_force() ###\n");
    int i1, i2, n, typ_i1, typ_i2;
    // double ebond, fbond;
 
@@ -107,7 +113,6 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
    int nbondlist = neighbor->nbondlist;
    int nlocal = atom->nlocal;
    // int nghost = atom->nghost;
-   int newton_bond = force->newton_bond;
 
    tagint *tag = atom->tag;
    
@@ -177,8 +182,8 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
       if (mol1 != mol2) error->all(FLERR, "There is a bond whose atoms belong to different molecules");
 
       delx = x[i1][0] - x[i2][0];
-      dely = x[i1][0] - x[i2][0];
-      delz = x[i1][0] - x[i2][0];
+      dely = x[i1][1] - x[i2][1];
+      delz = x[i1][2] - x[i2][2];
 
       delxsq = delx*delx;
       delysq = dely*dely;
@@ -194,6 +199,9 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
          there is no worry of double-counting a bond length */
 
       printf("Atom IDs (%d, %d) delx %f dely %f delz %f delxsq %f delysq %f delzsq %f dist %f\n", tag[i1], tag[i2], delx, dely, delz, delxsq, delysq, delzsq, dist);
+      // printf("Atom IDs (%d, %d) x1 %f y1 %f z1 %f x2 %f y2 %f z2 %f\n", tag[i1], tag[i2], x[i1][0], x[i1][1], x[i1][2], x[i2][0], x[i2][1], x[i2][2]);
+      
+      // printf("Atom IDs (%d, %d) f[i1][0]: %f f[i1][1]: %f f[i1][2]: %f f[i2][0]: %f f[i2][1]: %f f[i2][2]: %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
 
       /*
       if (isnan(molLengths[m - 1])) {
@@ -225,8 +233,251 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
 
    erestraint = 0;
    for (n = 0; n < nmols; n++) {
-      erestraint += k/2 * (molLengths[n] / equiLength - 1) * (molLengths[n] / equiLength - 1);
+      erestraint += k/2 * ((molLengths[n] * molLengths[n] / equiLength) - 2 * molLengths[n] + equiLength);
    }
+
+   printf("The total energy of the system is: %f\n\n", erestraint); 
+
+   double scalingFactors[max_glo_molID];
+   for(n = 0; n < max_glo_molID; n++) {
+      scalingFactors[n] = k * (molLengths[n] / equiLength - 1);
+   }
+
+   double scale;
+
+   double fx_i1;
+   double fy_i1;
+   double fz_i1;
+
+   /*
+   // The following is a debug last resort for dealing with force accumulation across timesteps
+   for (n = 0; n < nlocal; n++) {
+      f[n][0] = 0;
+      f[n][1] = 0;
+      f[n][2] = 0;
+   }
+   */
+
+
+   //Now that we have the molecule lengths, we can allocate forces
+   for (n = 0; n < nbondlist; n++){
+      i1 = bondlist[n][0];
+      i2 = bondlist[n][1];
+
+      mol1 = atom->molecule[i1];
+
+      scale = scalingFactors[mol1 - 1];
+
+      typ_i1 = type[i1];
+      typ_i2 = type[i2];
+
+      delx = x[i1][0] - x[i2][0];
+      dely = x[i1][1] - x[i2][1];
+      delz = x[i1][2] - x[i2][2];
+
+      delxsq = delx*delx;
+      delysq = dely*dely;
+      delzsq = delz*delz;
+
+      dist = sqrt(delxsq + delysq + delzsq);
+
+      printf("Atom IDs (%d, %d) types (%d, %d) scale %f delx %f dely %f delz %f dist %f\n", tag[i1], tag[i2], typ_i1, typ_i2, scale, delx, dely, delz, dist);
+      // printf("Atom IDs (%d, %d) x1 %f y1 %f z1 %f x2 %f y2 %f z2 %f\n", tag[i1], tag[i2], x[i1][0], x[i1][1], x[i1][2], x[i2][0], x[i2][1], x[i2][2]);
+
+      fx_i1 = 0;
+      fy_i1 = 0;
+      fz_i1 = 0;
+
+      /*
+      printf("\nHere are the forces on the atoms before assignment:\n");
+      printf("Atom IDs (%d, %d) f[i1][0]: %f f[i1][1]: %f f[i1][2]: %f f[i2][0]: %f f[i2][1]: %f f[i2][2]: %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
+      */
+
+
+      // This check is to avoid a division by zero error.
+      if (dist > 10e-18) {
+         fx_i1 = scale * delx / dist;
+         fy_i1 = scale * dely / dist;
+         fz_i1 = scale * delz / dist;
+      }
+
+      // This is for standardization, because we assume that
+      // the global type IDs for the atom of a molecule 
+      // are sequentially-ordered.
+      if (typ_i1 > typ_i2) {
+         fx_i1 *= -1;
+         fy_i1 *= -1;
+         fz_i1 *= -1;
+      }
+
+      f[i1][0] += fx_i1;
+      f[i1][1] += fy_i1;
+      f[i1][2] += fz_i1;
+
+      // If newton_bond == 1, then LAMMPS automatically adds
+      // -1*fx_i1 to f[i2][0], -1*fy_i1 to f[i2][1], -1*fz_1 to f[i2][2]
+      // Otherwise, we need to do this manually:
+      if (newton_bond == 0) {
+         f[i2][0] -= fx_i1;
+         f[i2][1] -= fy_i1;
+         f[i2][2] -= fz_i1;
+      }
+
+      printf("Atom IDs (%d, %d) fx_i1 %f fy_i1 %f fz_i1 %f\n", tag[i1], tag[i2], fx_i1, fy_i1, fz_i1);
+      
+      /*
+      printf("\nHere are the forces on the atoms after assignment:\n");
+      printf("Atom IDs (%d, %d) f[i1][0]: %f f[i1][1]: %f f[i1][2]: %f f[i2][0]: %f f[i2][1]: %f f[i2][2]: %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
+      */
+
+      // printf("Atom IDs (%d, %d) fx_i1 %f fy_i1 %f fz_i1 %f fx_i2 %f fy_i2 %f fz_i2 %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
+   }
+
+   printf("### Restraining forces called by post_force() is now complete. ###\n\n");
+
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixArcLengthRestraint::min_post_force(int vflag) {
+   int newton_bond = force->newton_bond;
+   if (newton_bond == 1) comm->reverse_comm(this);
+
+	
+   printf("\n### Begin calculations of forces called by min_post_force() ###\n");
+   // post_force(vflag);
+   int i1, i2, n, typ_i1, typ_i2;
+   // double ebond, fbond;
+
+   double **x = atom->x; // Get double pointer to atom positions
+   double **f = atom->f; // Get double pointer to atom forces
+   int *type = atom->type; // Get pointer to atom types
+   int **bondlist = neighbor->bondlist;
+   int nbondlist = neighbor->nbondlist;
+   int nlocal = atom->nlocal;
+   // int nghost = atom->nghost;
+
+   tagint *tag = atom->tag;
+   
+   int rank;
+   MPI_Comm_rank(world, &rank);
+
+   int ntimestep = update->ntimestep;
+
+   /*
+   if (rank == 0) {
+      printf("The current timestep is: %d\n", ntimestep);
+   }
+   */
+
+
+   int max_glo_molID;   
+   // For each processor, store the maximum local molecule ID in max_glo_molID
+   /*
+   for (n = 0; n < nlocal; n++) {
+      max_glo_molID = MAX(max_glo_molID, atom->molecule[n]);
+   }
+   */
+
+   for (n = 0; n < nbondlist; n++) {
+      i1 = bondlist[n][0];
+      max_glo_molID = MAX(max_glo_molID, atom->molecule[i1]);
+   }
+
+   // We take the highest value of max_glo_molID across all processors, and
+   // distribute that value to max_glo_molID to the rest of the processors
+   // (hence the usage of MPI_IN_PLACE as the send buffer)
+   MPI_Allreduce(MPI_IN_PLACE, &max_glo_molID, 1, MPI_INT, MPI_MAX, world);
+
+   // We want to create a double-type array with max_glo_molID elements
+   // to store the arc lengths of each molecule in the system
+   double molLengths[max_glo_molID] = {0};
+
+   double delx;
+   double dely;
+   double delz;
+
+   double delxsq;
+   double delysq;
+   double delzsq;
+
+   int mol1;
+   int mol2;
+
+   double dist;
+
+   /*
+   int size_Of_Cluster;
+   MPI_Comm_size(world, &size_Of_Cluster);
+   */
+
+   printf("\nBegin calculation of end-to-end distances per molecule.\n");
+   
+   for (n = 0; n < nbondlist; n++) {
+      dist = 0;
+      i1 = bondlist[n][0]; // Get proc index of first atom in bond index n
+      i2 = bondlist[n][1]; // Get proc index of second atom in bond index n
+
+      // Each processor appears in the bondlist of one and only one proc
+      // Therefore, we do not need to worry about under- or over- counting
+      mol1 = atom->molecule[i1];
+      mol2 = atom->molecule[i2];
+      if (mol1 != mol2) error->all(FLERR, "There is a bond whose atoms belong to different molecules");
+
+      delx = x[i1][0] - x[i2][0];
+      dely = x[i1][1] - x[i2][1];
+      delz = x[i1][2] - x[i2][2];
+
+      delxsq = delx*delx;
+      delysq = dely*dely;
+      delzsq = delz*delz;
+
+      // Calculate Euclidean distance of bond connecting atoms i1 and i2
+      dist = sqrt(delxsq + delysq + delzsq);
+
+      /* Use index mol1 - 1 because molecule ID indexing starts at 1
+         but C++ indexing starts at 0 */
+       molLengths[mol1 - 1] += dist;
+      /* For half neighbor lists, because each bond is only stored once,
+         there is no worry of double-counting a bond length */
+
+      printf("Atom IDs (%d, %d) delx %f dely %f delz %f delxsq %f delysq %f delzsq %f dist %f\n", tag[i1], tag[i2], delx, dely, delz, delxsq, delysq, delzsq, dist);
+      // printf("Atom IDs (%d, %d) x1 %f y1 %f z1 %f x2 %f y2 %f z2 %f\n", tag[i1], tag[i2], x[i1][0], x[i1][1], x[i1][2], x[i2][0], x[i2][1], x[i2][2]);
+
+      /*
+      if (isnan(molLengths[m - 1])) {
+         printf("atom IDs (%d, %d) of molecule %d from process %d\n", tag[i1], tag[i2], m, rank);
+      }
+      */
+
+   }
+   
+   /*
+   for (n = 0; n < nmols; n++) {
+      printf("Processor %d: the accumulated arc length of molecule with ID %d is %f\n",rank, n + 1, molLengths[n]);
+   }
+   */  
+
+
+   // Take all of the locally processor-owned arc lengths of each molecule, and 
+   // sum them up in place to get the total arc length for each molecule
+   MPI_Allreduce(MPI_IN_PLACE, &molLengths, nmols, MPI_DOUBLE, MPI_SUM, world);
+   
+   
+   if (rank == 0) {
+      for (n = 0; n < nmols; n++) {
+         printf("The length of molecule with ID %d is %f\n\n", n + 1, molLengths[n]);    
+      }
+   }
+   
+
+
+   erestraint = 0;
+   for (n = 0; n < nmols; n++) {
+      erestraint += k/2 * ((molLengths[n] * molLengths[n] / equiLength) - 2 * molLengths[n] + equiLength);
+   }
+
+   printf("The total energy of the system is: %f\n\n", erestraint); 
 
    double scalingFactors[max_glo_molID];
    for(n = 0; n < max_glo_molID; n++) {
@@ -262,10 +513,16 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
       dist = sqrt(delxsq + delysq + delzsq);
 
       printf("Atom IDs (%d, %d) types (%d, %d) scale %f delx %f dely %f delz %f dist %f\n", tag[i1], tag[i2], typ_i1, typ_i2, scale, delx, dely, delz, dist);
+      // printf("Atom IDs (%d, %d) x1 %f y1 %f z1 %f x2 %f y2 %f z2 %f\n", tag[i1], tag[i2], x[i1][0], x[i1][1], x[i1][2], x[i2][0], x[i2][1], x[i2][2]);
 
       fx_i1 = 0;
       fy_i1 = 0;
       fz_i1 = 0;
+
+      printf("\nHere are the forces on the atoms before assignment:\n");
+      printf("Atom IDs (%d, %d) f[i1][0]: %f f[i1][1]: %f f[i1][2]: %f f[i2][0]: %f f[i2][1]: %f f[i2][2]: %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
+
+
 
       // This check is to avoid a division by zero error.
       if (dist > 10e-18) {
@@ -296,23 +553,154 @@ void FixArcLengthRestraint::post_force(int /*vflag*/)
          f[i2][2] -= fz_i1;
       }
 
-      // printf("Atom IDs (%d, %d) fx_i1 %f fy_i1 %f fz_i1 %f\n", tag[i1], tag[i2], fx_i1, fy_i1, fz_i1);
+      printf("Atom IDs (%d, %d) fx_i1 %f fy_i1 %f fz_i1 %f\n", tag[i1], tag[i2], fx_i1, fy_i1, fz_i1);
+      
+      /*
+      printf("\nHere are the forces on the atoms after assignment:\n");
+      printf("Atom IDs (%d, %d) f[i1][0]: %f f[i1][1]: %f f[i1][2]: %f f[i2][0]: %f f[i2][1]: %f f[i2][2]: %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
+      */
 
       // printf("Atom IDs (%d, %d) fx_i1 %f fy_i1 %f fz_i1 %f fx_i2 %f fy_i2 %f fz_i2 %f\n", tag[i1], tag[i2], f[i1][0], f[i1][1], f[i1][2], f[i2][0], f[i2][1], f[i2][2]);
    }
 
-   printf("This iteration of calculating restraint forces is now complete.\n\n");
+   printf("### Restraining forces called by min_post_force complete. ###\n\n");
 
-}
-
-/* ---------------------------------------------------------------------- */
-
-void FixArcLengthRestraint::min_post_force(int vflag) {
-   post_force(vflag);
 }
 
 /* ------------------------------------------------------------------------- */
 
 double FixArcLengthRestraint::compute_scalar() {
+   printf("\n### Calculation of global restraint energy called with compute_scalar() ###\n");
+
+   int i1, i2, n;
+
+   double **x = atom->x; // Get double pointer to atom positions
+   int **bondlist = neighbor->bondlist;
+   int nbondlist = neighbor->nbondlist;
+
+   tagint *tag = atom->tag;
+   
+   int rank;
+   MPI_Comm_rank(world, &rank);
+
+   // int ntimestep = update->ntimestep;
+
+   /*
+   if (rank == 0) {
+      printf("The current timestep is: %d\n", ntimestep);
+   }
+   */
+
+
+   int max_glo_molID;   
+   // For each processor, store the maximum local molecule ID in max_glo_molID
+   /*
+   for (n = 0; n < nlocal; n++) {
+      max_glo_molID = MAX(max_glo_molID, atom->molecule[n]);
+   }
+   */
+
+   for (n = 0; n < nbondlist; n++) {
+      i1 = bondlist[n][0];
+      max_glo_molID = MAX(max_glo_molID, atom->molecule[i1]);
+   }
+
+   // We take the highest value of max_glo_molID across all processors, and
+   // distribute that value to max_glo_molID to the rest of the processors
+   // (hence the usage of MPI_IN_PLACE as the send buffer)
+   MPI_Allreduce(MPI_IN_PLACE, &max_glo_molID, 1, MPI_INT, MPI_MAX, world);
+
+   // We want to create a double-type array with max_glo_molID elements
+   // to store the arc lengths of each molecule in the system
+   double molLengths[max_glo_molID] = {0};
+
+   double delx;
+   double dely;
+   double delz;
+
+   double delxsq;
+   double delysq;
+   double delzsq;
+
+   int mol1;
+   int mol2;
+
+   double dist;
+
+   /*
+   int size_Of_Cluster;
+   MPI_Comm_size(world, &size_Of_Cluster);
+   */
+
+   printf("Begin calculation of end-to-end distances per molecule.\n");
+   
+   for (n = 0; n < nbondlist; n++) {
+      dist = 0;
+      i1 = bondlist[n][0]; // Get proc index of first atom in bond index n
+      i2 = bondlist[n][1]; // Get proc index of second atom in bond index n
+
+      // Each processor appears in the bondlist of one and only one proc
+      // Therefore, we do not need to worry about under- or over- counting
+      mol1 = atom->molecule[i1];
+      mol2 = atom->molecule[i2];
+      if (mol1 != mol2) error->all(FLERR, "There is a bond whose atoms belong to different molecules");
+
+      delx = x[i1][0] - x[i2][0];
+      dely = x[i1][1] - x[i2][1];
+      delz = x[i1][2] - x[i2][2];
+
+      delxsq = delx*delx;
+      delysq = dely*dely;
+      delzsq = delz*delz;
+
+      // Calculate Euclidean distance of bond connecting atoms i1 and i2
+      dist = sqrt(delxsq + delysq + delzsq);
+
+      /* Use index mol1 - 1 because molecule ID indexing starts at 1
+         but C++ indexing starts at 0 */
+       molLengths[mol1 - 1] += dist;
+      /* For half neighbor lists, because each bond is only stored once,
+         there is no worry of double-counting a bond length */
+
+      printf("Atom IDs (%d, %d) delx %f dely %f delz %f delxsq %f delysq %f delzsq %f dist %f\n", tag[i1], tag[i2], delx, dely, delz, delxsq, delysq, delzsq, dist);
+      // printf("Atom IDs (%d, %d) x1 %f y1 %f z1 %f x2 %f y2 %f z2 %f\n", tag[i1], tag[i2], x[i1][0], x[i1][1], x[i1][2], x[i2][0], x[i2][1], x[i2][2]);
+
+      /*
+      if (isnan(molLengths[m - 1])) {
+         printf("atom IDs (%d, %d) of molecule %d from process %d\n", tag[i1], tag[i2], m, rank);
+      }
+      */
+
+   }
+   
+   /*
+   for (n = 0; n < nmols; n++) {
+      printf("Processor %d: the accumulated arc length of molecule with ID %d is %f\n",rank, n + 1, molLengths[n]);
+   }
+   */  
+
+
+   // Take all of the locally processor-owned arc lengths of each molecule, and 
+   // sum them up in place to get the total arc length for each molecule
+   MPI_Allreduce(MPI_IN_PLACE, &molLengths, nmols, MPI_DOUBLE, MPI_SUM, world);
+   
+   
+   if (rank == 0) {
+      for (n = 0; n < nmols; n++) {
+         printf("The length of molecule with ID %d is %f\n\n", n + 1, molLengths[n]);    
+      }
+   }
+   
+
+
+   erestraint = 0;
+   for (n = 0; n < nmols; n++) {
+      erestraint += k/2 * ((molLengths[n] * molLengths[n] / equiLength) - 2 * molLengths[n] + equiLength);
+   }
+
+   printf("The total energy of the system is: %f\n\n", erestraint);  
+
+   printf("Calculation of global restraint energy complete.\n\n");
+	
    return erestraint;
 }
